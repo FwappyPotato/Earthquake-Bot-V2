27b8f9d1b66a9729d3e15ab28bd2a9efb913ba37
Ezra Kainz <ezra.kainz@gmail.com>
2021-11-05 14:47:34 -0700

    now it prints coords!

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import configparser
 import feedparser
 
 # Logging
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+#logger = logging.getLogger('discord')
+#logger.setLevel(logging.DEBUG)
+#handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+#handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+#logger.addHandler(handler)
 
 # Config
 config = configparser.ConfigParser()
 
 # Parser
-feed = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
+d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
+print(d.entries[0].where.coordinates)
 
 
 # Discord Bot
-client = discord.Client()
+#client = discord.Client()
 
-config.read('example.ini')
+#config.read('example.ini')
 
-
-client.run('your token here')
+#client.run('your token here')
