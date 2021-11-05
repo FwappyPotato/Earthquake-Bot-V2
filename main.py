10d006c1918cdda32782bc7c779c78a3f407f4fa
Ezra Kainz <ezra.kainz@gmail.com>
2021-11-05 10:22:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+# Config
+config = configparser.ConfigParser()
+
+# Parser
 feed = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
 
-config = configparser.ConfigParser()
+
+# Discord Bot
 client = discord.Client()
 
 config.read('example.ini')
