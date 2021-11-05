c42aee17b3c9ad6598ebba67366a72cd53a273e9
Ezra Kainz <ezra.kainz@gmail.com>
2021-11-05 15:22:43 -0700

    boom, now it prints distance

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import discord
 import configparser
 import feedparser
+import geopy.distance
 
 # Logging
 #logger = logging.getLogger('discord')
@@ -13,10 +14,16 @@ import feedparser
 # Config
 config = configparser.ConfigParser()
 
+waypoint = (30.8715, 120.2730)
+waypoint2 = (40.8715, 120.2230)
+
 # Parser
 d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
-print(d.entries[0].where.coordinates)
-
+quakecords = (d.entries[0].where.coordinates[1], d.entries[0].where.coordinates[0])
+print(quakecords)
+print(waypoint)
+distance = geopy.distance.distance(waypoint, quakecords).miles
+print("eathquake " + str(distance) + " miles away")
 
 # Discord Bot
 #client = discord.Client()
