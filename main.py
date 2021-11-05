579202a42b4268a34c5f02724bd8bfa57dd69724
Ezra Kainz <ezra.kainz@gmail.com>
2021-11-05 10:16:43 -0700

    1st push

---
+++ main.py
@@ -0,0 +1,21 @@
+import logging
+import discord
+import configparser
+import feedparser
+
+# Logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+feed = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
+
+config = configparser.ConfigParser()
+client = discord.Client()
+
+config.read('example.ini')
+
+
+client.run('your token here')
