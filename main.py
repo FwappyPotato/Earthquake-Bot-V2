9de5c1da105e6a2d5716d6530bf2d37c4be4b3b8
Ezra Kainz <ezra.kainz@gmail.com>
2021-11-06 10:31:53 -0700

    Ayy working discord bot!
    The discord bot now posts

--- main.py
+++ main.py
@@ -17,35 +17,38 @@ config = configparser.ConfigParser()
 config.read('conf.ini')
 
 bottoken = config['DEFAULT']['token']
-channelid = config['DEFAULT']['channel']
-print('sending to channel ' + channelid)
+channelid = int(config['DEFAULT']['channel'])
+print('sending to channel ' + str(channelid))
 looptime = 3
 
 waypoint = (30.8715, 120.2730)
 waypoint2 = (40.8715, 120.2230)
+
 
 # Discord Bot
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('logged in as {0.user}'.format(client))
     print('------')
+    d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
+    quakecords = (d.entries[0].where.coordinates[1], d.entries[0].where.coordinates[0])
     while True:
         # Parser
         d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
+        oldquakecords = quakecords
         quakecords = (d.entries[0].where.coordinates[1], d.entries[0].where.coordinates[0])
-        print(quakecords)
-        print(waypoint)
-        distance = geopy.distance.distance(waypoint, quakecords).miles
-        print('eathquake ' + str(distance) + ' miles away')
-        #^ this part works great ^
-        if distance < 10000:
-            print('eathquake!!')
-            channel = client.get_channel(channelid)
-            await channel.send('earthquake!!')
+        if quakecords != oldquakecords:
+            print(quakecords)
+            # Find Distance
+            distance = geopy.distance.distance(waypoint, quakecords).miles
+            print('eathquake ' + str(distance) + ' miles away')
+            # Send Message if within distance
+            if distance < 100:
+                print('eathquake!!')
+                channel = client.get_channel(channelid)
+                await channel.send('earthquake!!')
         time.sleep(looptime)
-       
-
 
 client.run(bottoken)
