23dc16ff08baf34e4fc6629925e906dca4bb00c0
Ezra Kainz <ezra.kainz@gmail.com>
2021-11-05 18:47:15 -0700

    idk what im doing
    This is gross and I know it

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 import configparser
 import feedparser
 import geopy.distance
+import time
 
 # Logging
 #logger = logging.getLogger('discord')
@@ -13,21 +14,39 @@ import geopy.distance
 
 # Config
 config = configparser.ConfigParser()
+config.read('conf.ini')
+
+bottoken = config['DEFAULT']['token']
+channelid = config['DEFAULT']['channel']
+print(channelid)
+looptime = 3
 
 waypoint = (30.8715, 120.2730)
 waypoint2 = (40.8715, 120.2230)
 
-# Parser
-d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
-quakecords = (d.entries[0].where.coordinates[1], d.entries[0].where.coordinates[0])
-print(quakecords)
-print(waypoint)
-distance = geopy.distance.distance(waypoint, quakecords).miles
-print("eathquake " + str(distance) + " miles away")
-
 # Discord Bot
-#client = discord.Client()
 
-#config.read('example.ini')
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+        while True:
+            # Parser
+            d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
+            quakecords = (d.entries[0].where.coordinates[1], d.entries[0].where.coordinates[0])
+            print(quakecords)
+            print(waypoint)
+            distance = geopy.distance.distance(waypoint, quakecords).miles
+            print("eathquake " + str(distance) + " miles away")
+            if distance < 10000:
+                print("eathquake!!")
+                channel = client.get_channel(channelid)
+                await channel.send("earthquake!!")
+            time.sleep(looptime)
+            
+        
 
-#client.run('your token here')
+client = MyClient()
+client.run(bottoken)
