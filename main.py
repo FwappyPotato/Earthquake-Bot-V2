eb5082d705331fe387d08ffe9ac5a65705a483f7
Ezra Kainz <ezra.kainz@gmail.com>
2021-11-06 11:17:30 -0700

    some optomizations
    use modified time instead of redownloading file every time, still need to verify data seems it gets updated multiple times for every earhquake. Might be nice to at some point update last sent message is data changes

--- main.py
+++ main.py
@@ -18,13 +18,14 @@ config.read('conf.ini')
 
 bottoken = config['DEFAULT']['token']
 channelid = int(config['DEFAULT']['channel'])
-print('sending to channel ' + str(channelid))
-looptime = 3
+
+looptime = 15
 
 waypoint = (30.8715, 120.2730)
 waypoint2 = (40.8715, 120.2230)
 
-
+print('sending to channel ' + str(channelid))
+print('------')
 # Discord Bot
 client = discord.Client()
 
@@ -33,22 +34,26 @@ async def on_ready():
     print('logged in as {0.user}'.format(client))
     print('------')
     d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
-    quakecords = (d.entries[0].where.coordinates[1], d.entries[0].where.coordinates[0])
+    modified = d.modified
     while True:
         # Parser
-        d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
-        oldquakecords = quakecords
+        d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom', modified=modified)
+        if d.status == 304:
+            #print('unchanged')
+            time.sleep(looptime)
+            continue
+        modified = d.modified
+        print('updated at ' + modified)
         quakecords = (d.entries[0].where.coordinates[1], d.entries[0].where.coordinates[0])
-        if quakecords != oldquakecords:
-            print(quakecords)
-            # Find Distance
-            distance = geopy.distance.distance(waypoint, quakecords).miles
-            print('eathquake ' + str(distance) + ' miles away')
-            # Send Message if within distance
-            if distance < 100:
-                print('eathquake!!')
-                channel = client.get_channel(channelid)
-                await channel.send('earthquake!!')
+        print(quakecords)
+        # Find Distance
+        distance = geopy.distance.distance(waypoint, quakecords).miles
+        print('eathquake ' + str(distance) + ' miles away')
+        # Send Message if within distance
+        if distance < 100:
+            print('eathquake!!')
+            channel = client.get_channel(channelid)
+            await channel.send('earthquake!!')
         time.sleep(looptime)
 
 client.run(bottoken)
