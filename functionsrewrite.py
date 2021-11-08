acc3e4cdae8aa0833aac950e56065519c4a351ff
Ezra Kainz <ezra.kainz@gmail.com>
2021-11-08 14:56:29 -0800

    Update functionsrewrite.py
    continuing rewrite

--- functionsrewrite.py
+++ functionsrewrite.py
@@ -11,15 +11,23 @@ print('--updated at ' + modified)
 
 # Filter
 quakecords = (d.entries[0].where.coordinates[1], d.entries[0].where.coordinates[0])
-distance = geopy.distance.distance(waypoint, quakecords).miles
-print(str(d.entries[0].tags[1].term) + ' earthquake ' + str("%.2f" % distance) + ' miles away ' + str(quakecords))
-if distance < pingdist: #configure in a way that allows n number of distances to be calculated
+distance1 = geopy.distance.distance(waypoint1, quakecords).miles
+distance2 = geopy.distance.distance(waypoint2, quakecords).miles
+distance3 = geopy.distance.distance(waypoint3, quakecords).miles
+print(str(d.entries[0].tags[1].term) + ' earthquake ' + str("%.2f" % distance1) + ' miles away ' + str(quakecords))
+if distance1 < pingdist or distance2 < pingdist or distance3 < pingdist: #configure in a way that allows n number of distances to be calculated
+    # print(Nearby)
     # call ID checker
 
 #ID Checker - If (matching - update message)/Else (send new message)
 
 if d.entries[0].id == oldid
-#could look for previous ID in feed instead of only entries index 1, then we can ignore non consequectial eathquakes
+    #call editmessage
+#call newmessage
+
+#could look for previous ID in feed instead of only entries index 1, then we can continue updating even if there are consecutive eathquakes
+
+
 
 
 # Discord Bot
