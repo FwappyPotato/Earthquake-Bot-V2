3947dfb87008d6197eae0bf8ee5c3971d0ee3590
Ezra Kainz <ezra.kainz@gmail.com>
2021-11-11 10:18:32 -0800

    multi-waypoint

--- main.py
+++ main.py
@@ -25,15 +25,23 @@ roleid = config['default']['roleid']
 
 looptime = int(config['default']['frequency'])
 
-waypoint = (float(config['default']['waypointlat']), float(config['default']['waypointlon']))
-waypointname = config['default']['waypointname']
+waypoint1 = (float(config['default']['waypoint1lat']), float(config['default']['waypoint1lon']))
+waypoint1name = config['default']['waypoint1name']
+waypoint2 = (float(config['default']['waypoint2lat']), float(config['default']['waypoint2lon']))
+waypoint2name = config['default']['waypoint2name']
+waypoint3 = (float(config['default']['waypoint3lat']), float(config['default']['waypoint3lon']))
+waypoint3name = config['default']['waypoint3name']
+
 pingdist = int(config['default']['distance'])
 
 print('channelid ' + str(channelid))
 print('roleid ' + str(roleid))
-print('waypoint ' + waypointname + ' ' + str(waypoint) + ' ' + str(type(waypoint)))
-print('pingdist ' + str(pingdist))
-print('looptime ' + str(looptime))
+print('waypoint1 ' + waypoint1name + ' ' + str(waypoint1) + ' ' + str(type(waypoint1)))
+print('waypoint2 ' + waypoint2name + ' ' + str(waypoint2) + ' ' + str(type(waypoint2)))
+print('waypoint3 ' + waypoint3name + ' ' + str(waypoint3) + ' ' + str(type(waypoint3)))
+print('pingdist ' + str(pingdist) + ' miles')
+print('looptime ' + str(looptime) + ' seconds')
+
 # Discord Bot
 client = discord.Client()
 
@@ -62,12 +70,14 @@ async def on_ready(): # Yes this is bad, I know... Tell me how to do it better
         print('--updated at ' + modified)
         quakecords = (float(d.entries[0].where.coordinates[1]), float(d.entries[0].where.coordinates[0]))
         # Find Distance
-        distance = geopy.distance.distance(waypoint, quakecords).miles
-        print(str("%.2f" % distance) + ' miles away ' + ' --- ' + str(d.entries[0].title) + ' ' + str(quakecords))
+        distance1 = geopy.distance.distance(waypoint1, quakecords).miles
+        distance2 = geopy.distance.distance(waypoint2, quakecords).miles
+        distance3 = geopy.distance.distance(waypoint3, quakecords).miles
+        print(str("%.2f" % distance1) + ' miles away ' + ' --- ' + str(d.entries[0].title) + ' ' + str(quakecords))
         # Send Message if within distance
-        if distance < pingdist:
-            print('Under ' + str(pingdist) + ' sending Discord Message!!')
-            await channel.send('<@&' + roleid + '>\n**' + str(d.entries[0].title) + '**\n`' + str("%.2f" % distance) + '` miles from ' + waypointname + '\n\n' + 'Time: `' + d.entries[0].updated + '`\nDepth: `' + d.entries[0].georss_elev + ' Meters`\n' + d.entries[0].link)
+        if distance1 < pingdist or distance2 < pingdist or distance3 < pingdist:
+            print('Under ' + str(pingdist) + ' miles sending Discord Message!!')
+            await channel.send('<@&' + roleid + '>\n**' + str(d.entries[0].title) + '**\n`' + str("%.2f" % distance1) + '` miles from ' + waypoint1name + '\n`' + str("%.2f" % distance2) + '` miles from ' + waypoin2tname + '\n`' +str("%.2f" % distance3) + '` miles from ' + waypoint3name + '\n\n' + 'Time: `' + d.entries[0].updated + '`\nDepth: `' + d.entries[0].georss_elev + ' Meters`\nLocation: `' + str(quakecords) + '`\n' + d.entries[0].link)
         oldid = d.entries[0].id
         await asyncio.sleep(looptime)
 
